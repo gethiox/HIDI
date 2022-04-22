a6848e13ec2b6433ebf47c644148d96ac85e3a9f
Sławomir Kur <gethiox@gmail.com>
2022-04-22 21:06:58 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-GOOS=linux GOARCH=arm GOARM=6 go build -o ./builds/hidi cmd/main.go
+echo "$(date) - compiling..."
+GOOS=linux GOARCH=arm GOARM=6 go build -o ./builds/hidi cmd/*.go
 if [[  $? -ne 0 ]]; then
-  read -n 1
+  echo "$(date) - compilation failed"
+  exit 1
 fi
+
+echo "$(date) - compilation done"
+exit 0
